18a344f526b7c2af3871284863d366eb7d1182a3
jayseregon <jayseregon@gmail.com>
2024-09-19 18:10:05 -0400

    policy styling fix

--- src/app/policies/[policy]/page.tsx
+++ src/app/policies/[policy]/page.tsx
@@ -24,13 +24,11 @@ export default function PolicyPage() {
 
   return (
     // <div>
-    <div className="w-full max-w-4xl mx-auto p-4 sm:p-6 md:p-8">
-      {/* <h1 className="text-purple-800 dark:text-purple-300 mb-3"> */}
-      <h1 className="text-purple-800 dark:text-purple-300 mb-3 text-2xl sm:text-3xl md:text-4xl">
+    <div className="w-full max-w-4xl md:max-w-5xl mx-auto">
+      <h1 className="text-purple-800 dark:text-purple-300 mb-3">
         {t(`${policyData.translationId}.title`)}
       </h1>
-      {/* <h2 className="text-5xl font-bold max-w-xl mx-auto"> */}
-      <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold max-w-xl mx-auto">
+      <h2 className="text-5xl font-bold max-w-xl mx-auto">
         {t(`${policyData.translationId}.heroTitle`)}
       </h2>
 
--- src/app/policies/layout.tsx
+++ src/app/policies/layout.tsx
@@ -6,7 +6,6 @@ export default function PolicyLayout({
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block text-center justify-center px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 pt-10 w-full">
-        {/* <div className="inline-block text-center justify-center px-16 sm:px-20 md:px-32 pt-10"> */}
         {children}
       </div>
     </section>
