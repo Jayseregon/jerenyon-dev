34733d80cf88e7de21f649ed825c56863831224f
jayseregon <jayseregon@gmail.com>
2024-09-29 21:47:29 -0400

    landing page adjustment tryouts #8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,9 @@ export default function RootPage() {
   const nonce = headers().get("x-nonce");
 
   return (
-    <div className="relative h-screen w-screen overflow-hidden grid grid-rows-[minmax(0,2fr)_minmax(0,1fr)] sm:grid-rows-[minmax(0,1fr)_minmax(0,auto)]">
-      {/* Parallax Image */}
-      <div className="row-span-1">
-        <ParallaxImage
-          height="100%"
-          nonce={nonce ?? undefined}
-          width="100%"
-        />
-      </div>
-
+    <div className="relative h-screen w-screen overflow-hidden grid grid-rows-[minmax(0,1fr)_minmax(0,2fr)] sm:grid-rows-[minmax(0,auto)_minmax(0,1fr)]">
       {/* Hero Title and Subtitle */}
-      <div className="flex flex-col-reverse items-center justify-center p-2 border-2 border-lime-500">
+      <div className="flex flex-col items-center justify-center p-2 border-2 border-lime-500 pt-16">
         <div className="text-center w-full max-w-full overflow-hidden border-2">
           <h2
             className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-foreground mx-auto border-2"
@@ -39,6 +30,15 @@ export default function RootPage() {
             {t("hero.subtitle")}
           </h3>
         </div>
+      </div>
+
+      {/* Parallax Image */}
+      <div className="row-span-1">
+        <ParallaxImage
+          height="100%"
+          nonce={nonce ?? undefined}
+          width="100%"
+        />
       </div>
     </div>
   );
