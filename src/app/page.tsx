354b5daac82a8313e92537314cbc12b72b1be726
jayseregon <jayseregon@gmail.com>
2024-09-29 21:55:55 -0400

    landing page adjustment tryouts #9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,20 @@ export default function RootPage() {
   const nonce = headers().get("x-nonce");
 
   return (
-    <div className="relative h-screen w-screen overflow-hidden grid grid-rows-[minmax(0,1fr)_minmax(0,2fr)] sm:grid-rows-[minmax(0,auto)_minmax(0,1fr)]">
+    <div className="relative h-screen w-screen overflow-hidden grid grid-rows-[minmax(0,1fr)_minmax(0,auto)]">
+      {/* Parallax Image */}
+      <div className="row-span-1">
+        <ParallaxImage
+          height="90%"
+          nonce={nonce ?? undefined}
+          width="90%"
+          marginTopClass="mt-12"
+        />
+      </div>
+
       {/* Hero Title and Subtitle */}
-      <div className="flex flex-col items-center justify-center p-2 border-2 border-lime-500 pt-16">
-        <div className="text-center w-full max-w-full overflow-hidden border-2">
+      <div className="flex flex-col-reverse items-center justify-center p-2 border-2 border-lime-500">
+        <div className="text-center w-full max-w-full overflow-hidden">
           <h2
             className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-foreground mx-auto border-2"
             id="hero-title">
@@ -25,20 +35,11 @@ export default function RootPage() {
           </h2>
         </div>
 
-        <div className="text-center w-full mt-2 max-w-full overflow-hidden border-2">
+        <div className="text-center w-full mt-2 max-w-full overflow-hidden">
           <h3 className="text-purple-800 dark:text-purple-300 border-2">
             {t("hero.subtitle")}
           </h3>
         </div>
-      </div>
-
-      {/* Parallax Image */}
-      <div className="row-span-1">
-        <ParallaxImage
-          height="100%"
-          nonce={nonce ?? undefined}
-          width="100%"
-        />
       </div>
     </div>
   );
