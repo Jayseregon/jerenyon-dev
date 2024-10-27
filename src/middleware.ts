b4674a42274b1ae67ef18a9898d0708bd4476dec
jayseregon <jayseregon@gmail.com>
2024-10-27 15:40:23 -0400

    update middleware csp #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,9 @@ function cspMiddleware(req: NextRequest): NextResponse {
 
   const cspHeader = `
   default-src 'self' https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev;
-  script-src 'self' 'nonce-${nonce}' 'unsafe-inline' blob: https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live https://vercel.live/_next-live/feedback;
-  style-src 'self' 'nonce-${nonce}' 'unsafe-inline' https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://app.termageddon.com https://vercel.live;
-  img-src 'self' blob: data: https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://app.usercentrics.eu;
+  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' blob: https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live https://vercel.live/_next-live/feedback;
+  style-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://app.termageddon.com https://vercel.live;
+  img-src 'self' 'strict-dynamic' blob: data: https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://app.usercentrics.eu;
   font-src 'self' https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev;
   connect-src 'self' https://jerenyon.dev https://www.jerenyon.dev https://*.jerenyon.dev https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live;
   object-src 'none';
