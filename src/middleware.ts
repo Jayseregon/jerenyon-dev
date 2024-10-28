0a07aa150336188f18efec213fa86be3cf041f52
jayseregon <jayseregon@gmail.com>
2024-10-27 20:17:57 -0400

    try and fix CSP errors #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,18 @@ function cspMiddleware(req: NextRequest): NextResponse {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
   const cspHeader = `
-  default-src 'self' https://www.jerenyon.dev;
-  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live;
-  style-src 'self' 'nonce-${nonce}' https://www.jerenyon.dev https://app.termageddon.com https://vercel.live;
-  img-src 'self' blob: data: https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net;
-  font-src 'self' https://www.jerenyon.dev;
-  connect-src 'self' https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live;;
-  object-src 'none';
-  base-uri 'self' https://www.jerenyon.dev;
-  form-action 'self' https://www.jerenyon.dev;
-  frame-src 'self' https://www.google.com;
-  frame-ancestors 'none';
-  upgrade-insecure-requests;
+    default-src 'self' https://www.jerenyon.dev https://*.jerenyon.dev;
+    script-src 'self' 'nonce-${nonce}' blob: https://www.jerenyon.dev https://*.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live;
+    style-src 'self' 'nonce-${nonce}' https://www.jerenyon.dev https://*.jerenyon.dev https://app.termageddon.com https://vercel.live;
+    img-src 'self' blob: data: https://www.jerenyon.dev https://*.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://app.usercentrics.eu;
+    font-src 'self' https://www.jerenyon.dev https://*.jerenyon.dev;
+    connect-src 'self' https://www.jerenyon.dev https://*.jerenyon.dev https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live;
+    object-src 'none';
+    base-uri 'self' https://www.jerenyon.dev https://*.jerenyon.dev;
+    form-action 'self' https://www.jerenyon.dev https://*.jerenyon.dev;
+    frame-src 'self' https://www.google.com;
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
   `
     .replace(/\s{2,}/g, " ")
     .trim();
