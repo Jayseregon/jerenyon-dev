f4162745c7acf1d25779d2a27bcc8133c5b5f389
jayseregon <jayseregon@gmail.com>
2024-10-29 21:12:12 -0400

    temporary set unsafe-eval & unsafe-inline for staging

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,8 +24,8 @@ function cspMiddleware(req: NextRequest): NextResponse {
 
   const cspHeader = `
   default-src 'self' https://www.jerenyon.dev;
-  script-src 'self' 'nonce-${nonce}' https://www.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live https://jerenyon-dev-remote-pull.b-cdn.net;
-  style-src 'self' 'nonce-${nonce}' https://www.jerenyon.dev https://fonts.googleapis.com https://app.termageddon.com https://vercel.live;
+  script-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://www.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live https://jerenyon-dev-remote-pull.b-cdn.net;
+  style-src 'self' 'nonce-${nonce}' 'unsafe-inline' https://www.jerenyon.dev https://fonts.googleapis.com https://app.termageddon.com https://vercel.live;
   img-src 'self' blob: data: https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://jerenyon-dev-remote-pull.b-cdn.net;
   font-src 'self' https://www.jerenyon.dev https://fonts.gstatic.com;
   connect-src 'self' https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://unpkg.com https://jerenyon-dev-remote-pull.b-cdn.net https://fonts.gstatic.com;
