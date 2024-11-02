4cdee96652e2f1f5f6e875908b609449407237f8
jayseregon <jayseregon@gmail.com>
2024-11-02 11:14:37 -0400

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ function cspMiddleware(req: NextRequest): NextResponse {
   style-src 'self' 'nonce-${nonce}' https://www.jerenyon.dev https://app.termageddon.com https://vercel.live;
   img-src 'self' blob: data: https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://vercel.live https://vercel.com;
   font-src 'self' https://www.jerenyon.dev https://vercel.live https://assets.vercel.com;
-  connect-src 'self' https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live wss://ws-us3.pusher.com;
+  connect-src 'self' https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live https://jerenyon-dev-remote-pull.b-cdn.net wss://ws-us3.pusher.com;
   object-src 'none';
   base-uri 'self' https://www.jerenyon.dev;
   form-action 'self' https://www.jerenyon.dev;
@@ -44,7 +44,7 @@ function splineCspMiddleware(req: NextRequest): NextResponse {
   style-src 'self' 'nonce-${nonce}' 'unsafe-inline' https://www.jerenyon.dev https://app.termageddon.com https://vercel.live;
   img-src 'self' blob: data: https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://vercel.live https://vercel.com;
   font-src 'self' https://www.jerenyon.dev https://vercel.live https://assets.vercel.com;
-  connect-src 'self' https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live wss://ws-us3.pusher.com;
+  connect-src 'self' https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live https://jerenyon-dev-remote-pull.b-cdn.net wss://ws-us3.pusher.com;
   object-src 'none';
   base-uri 'self' https://www.jerenyon.dev;
   form-action 'self' https://www.jerenyon.dev;
