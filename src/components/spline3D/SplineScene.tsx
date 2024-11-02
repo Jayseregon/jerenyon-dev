08c8e277088ba33cea8ea82b50dbfd41f36a49f8
jayseregon <jayseregon@gmail.com>
2024-11-01 20:00:44 -0400

    try new responsive scene

--- src/components/spline3D/SplineScene.tsx
+++ src/components/spline3D/SplineScene.tsx
@@ -8,14 +8,28 @@ import { NonceContext } from "@/src/app/providers";
 export default function SplineScene() {
   const nonce = useContext(NonceContext);
 
+  interface SplineMouseEvent {
+    target: {
+      name: string;
+    };
+  }
+
+  function onSplineMouseDown(e: SplineMouseEvent) {
+    if (e.target.name === "button-estimate") {
+      console.log("I have been clicked!");
+    }
+  }
+
   return (
     <div className="w-full h-full bg-background">
       <Spline
         className="bg-transparent"
         nonce={nonce}
-        // scene="https://jerenyon-dev-remote-pull.b-cdn.net/spline-scene/landing-codeblock-scene.splinecode"
-        scene="/spline/scene.splinecode"
+        scene="https://jerenyon-dev-remote-pull.b-cdn.net/spline-scene/landing-codeblock-scene-3.splinecode"
+        // onSplineMouseDown={onSplineMouseDown}
       />
     </div>
   );
 }
+
+// button-estimate
