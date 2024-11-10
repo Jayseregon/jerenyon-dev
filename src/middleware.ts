4dae671038228db52bf19349faa0321f47246d03
jayseregon <jayseregon@gmail.com>
2024-11-10 11:52:23 -0500

    hotfix(middleware) adjust CSP #8

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,84 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
 
-function createCspHeader(nonce: string, isLandingPage: boolean): string {
-  const baseCSP = `
-    default-src 'self' https://jerenyon.dev https://www.jerenyon.dev;
-    img-src 'self' blob: data: https://jerenyon.dev https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://app.usercentrics.eu;
-    font-src 'self' https://jerenyon.dev https://www.jerenyon.dev;
-    object-src 'none';
-    base-uri 'self' https://jerenyon.dev https://www.jerenyon.dev;
-    form-action 'self' https://jerenyon.dev https://www.jerenyon.dev;
-    frame-src 'self' https://www.google.com;
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-  `;
-
-  const commonScriptSources = `
-    https://jerenyon.dev 
-    https://www.jerenyon.dev 
-    https://www.google.com 
-    https://www.gstatic.com 
-    https://app.termageddon.com 
-    https://privacy-proxy.usercentrics.eu 
-    https://app.usercentrics.eu 
-    https://vercel.live 
-    https://vercel.live/_next-live/feedback 
-    https://unpkg.com
-  `
-    .replace(/\s+/g, " ")
-    .trim();
-
-  const commonStyleSources = `
-    https://jerenyon.dev 
-    https://www.jerenyon.dev 
-    https://app.termageddon.com 
-    https://vercel.live
-  `
-    .replace(/\s+/g, " ")
-    .trim();
-
-  const commonConnectSources = `
-    https://jerenyon.dev 
-    https://www.jerenyon.dev 
-    https://app.termageddon.com 
-    https://privacy-proxy.usercentrics.eu 
-    https://app.usercentrics.eu 
-    https://api.usercentrics.eu 
-    https://vercel.live
-  `
-    .replace(/\s+/g, " ")
-    .trim();
-
-  const splineConnectSources = `
-    https://unpkg.com/@splinetool/modelling-wasm@* 
-    https://unpkg.com/@splinetool/* 
-    https://fonts.gstatic.com 
-    wss://ws-us3.pusher.com
-  `
-    .replace(/\s+/g, " ")
-    .trim();
-
-  // Define script-src, style-src, and connect-src based on the page type
-  const scriptSrc = `'self' 'nonce-${nonce}' 'unsafe-eval' 'strict-dynamic' blob: ${commonScriptSources}`;
-  const styleSrc = `'self' 'nonce-${nonce}' ${isLandingPage ? "'unsafe-eval'" : ""} ${commonStyleSources}`;
-  const connectSrc = `'self' ${commonConnectSources} ${isLandingPage ? splineConnectSources : ""}`;
-
-  const cspExtras = `
-    script-src ${scriptSrc};
-    style-src ${styleSrc};
-    connect-src ${connectSrc};
-  `;
-
-  const cspHeader = `${baseCSP}${cspExtras}`.replace(/\s{2,}/g, " ").trim();
-
-  return cspHeader;
-}
-
-function cspMiddleware(req: NextRequest): NextResponse {
+function cspMiddleware(req: NextRequest, isLandingPage: boolean): NextResponse {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-  const isLandingPage =
-    req.nextUrl.pathname === "/" || req.nextUrl.pathname === "";
-  const cspHeader = createCspHeader(nonce, isLandingPage);
+
+  const cspHeaderLandingPage = `
+  default-src 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  script-src 'self' 'nonce-${nonce}' 'unsafe-eval' 'strict-dynamic' blob: https://jerenyon.dev https://www.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live https://vercel.live/_next-live/feedback;
+  style-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://jerenyon.dev https://www.jerenyon.dev https://app.termageddon.com https://vercel.live;
+  img-src 'self' blob: data: https://jerenyon.dev https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://app.usercentrics.eu;
+  font-src 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  connect-src 'self' https://jerenyon.dev https://www.jerenyon.dev https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live https://unpkg.com https://fonts.gstatic.com wss://ws-us3.pusher.com;
+  object-src 'none';
+  base-uri 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  form-action 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  frame-src 'self' https://www.google.com;
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+  `
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const cspHeaderOtherPages = `
+  default-src 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' blob: https://jerenyon.dev https://www.jerenyon.dev https://www.google.com https://www.gstatic.com https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://vercel.live https://vercel.live/_next-live/feedback;
+  style-src 'self' 'nonce-${nonce}' https://jerenyon.dev https://www.jerenyon.dev https://app.termageddon.com https://vercel.live;
+  img-src 'self' blob: data: https://jerenyon.dev https://www.jerenyon.dev https://jerenyon-dev-cdn.b-cdn.net https://app.usercentrics.eu;
+  font-src 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  connect-src 'self' https://jerenyon.dev https://www.jerenyon.dev https://app.termageddon.com https://privacy-proxy.usercentrics.eu https://app.usercentrics.eu https://api.usercentrics.eu https://vercel.live;
+  object-src 'none';
+  base-uri 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  form-action 'self' https://jerenyon.dev https://www.jerenyon.dev;
+  frame-src 'self' https://www.google.com;
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+  `
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const cspHeader = isLandingPage ? cspHeaderLandingPage : cspHeaderOtherPages;
 
   const requestHeaders = new Headers(req.headers);
 
@@ -90,10 +49,6 @@ function cspMiddleware(req: NextRequest): NextResponse {
     },
   });
 
-  // Ensure no caching to prevent stale CSP headers
-  response.headers.set("Cache-Control", "no-store, must-revalidate, max-age=0");
-  response.headers.set("Pragma", "no-cache");
-  response.headers.set("Expires", "0");
   response.headers.set("Content-Security-Policy", cspHeader);
 
   return response;
@@ -101,9 +56,13 @@ function cspMiddleware(req: NextRequest): NextResponse {
 
 export function middleware(req: NextRequest) {
   const isDev = process.env.NODE_ENV === "development";
+  const pathname = req.nextUrl.pathname;
+
+  // Check if the route matches the landing page ("/") or other pages.
+  const isLandingPage = pathname === "/";
 
   if (!isDev) {
-    return cspMiddleware(req);
+    return cspMiddleware(req, isLandingPage);
   } else {
     return NextResponse.next();
   }
@@ -111,7 +70,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/", // Root
+    "/", // Landing page
     "/((?!_next|_vercel|.*\\..*).*)", // Exclude certain paths
     "/((?!api|_next/static|_next/image|static|favicon.ico|favicon.png|favicon-light.png|favicon-dark.png).*)",
   ],
