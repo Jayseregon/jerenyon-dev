aac3b35d72d397364a251757e2c611b2d65620aa
jayseregon <jayseregon@gmail.com>
2024-11-17 16:05:19 -0500

    trying perfs on Spline SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { useTranslations } from "next-intl";
 import React, { Suspense } from "react";
 
-import SplineScene from "@/src/components/spline3D/SplineScene";
+import SplineScene from "@/components/spline3D/SplineScene";
+import SplineScene_ssr from "@/components/spline3D/SplineScene_ssr";
 
 import Loading from "./loading";
 
@@ -13,14 +14,13 @@ export default function RootPage() {
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-background">
       <Suspense fallback={<Loading />}>
-        <SplineScene />
+        <SplineScene_ssr />
       </Suspense>
       {/* Hero Title and Subtitle */}
       <div className="absolute bottom-0 w-full flex flex-col items-center justify-center pb-5 z-10">
         <h2
           className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center text-foreground"
-          id="hero-title"
-        >
+          id="hero-title">
           {t("hero.title")}
         </h2>
         <p className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-secondary text-center">
--- src/components/spline3D/SplineScene_ssr.tsx
+++ src/components/spline3D/SplineScene_ssr.tsx
@@ -1,18 +1,18 @@
-// import React from "react";
-// import Spline from "@splinetool/react-spline/next";
+import React from "react";
+import Spline from "@splinetool/react-spline/next";
 
-// import SplineEventHandler from "./SplineEventHandler";
+import SplineEventHandler from "./SplineEventHandler";
 
-// const SplineScene = () => {
-//   return (
-//     <>
-//       <Spline
-//         className="w-full h-full"
-//         scene="/spline/hero-3d-scene.splinecode"
-//       />
-//       <SplineEventHandler />
-//     </>
-//   );
-// };
+const SplineScene_ssr = () => {
+  return (
+    <>
+      <Spline
+        className="w-full h-full"
+        scene="/spline/hero-3d-scene.splinecode"
+      />
+      <SplineEventHandler />
+    </>
+  );
+};
 
-// export default React.memo(SplineScene);
+export default React.memo(SplineScene_ssr);
