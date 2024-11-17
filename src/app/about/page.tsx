4a62d611c272ea92bba581dd9fcde67a45a323c2
jayseregon <jayseregon@gmail.com>
2024-11-17 14:50:19 -0500

    adjust about sections padding

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -42,7 +42,7 @@ export default function AboutPage() {
         pageTitle={t("title")}
       />
       <div className="space-y-20 mt-10">
-        <div className="flex flex-col max-w-lg md:max-w-3xl mx-auto space-y-20">
+        <div className="flex flex-col max-w-lg md:max-w-3xl mx-auto space-y-20 px-5">
           {sectionData.map((section, index) => (
             <AboutCard key={index} {...section} />
           ))}
