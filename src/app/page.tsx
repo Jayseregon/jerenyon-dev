568cb621adab57b8fc87389da8fcc4fc7f458db6
jayseregon <jayseregon@gmail.com>
2025-01-03 10:11:19 -0500

    reverting to Spline SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,25 @@
 import React, { Suspense } from "react";
 // import { useReportWebVitals } from "next/web-vitals";
 
-import SplineScene from "@/components/spline3D/SplineScene";
-// import SplineScene_ssr from "@/components/spline3D/SplineScene_ssr";
+// import SplineScene from "@/components/spline3D/SplineScene";
+// keeping these for future reference and usage
+import SplineScene_ssr from "@/components/spline3D/SplineScene_ssr";
 // import SplineSceneView from "@/components/spline3D/SplineScene_Viewer";
 import { Hero } from "@/components/ui/Hero";
 
 import Loading from "./loading";
 
 export default function RootPage() {
+  // keeping this for future reference and usage
   // useReportWebVitals((metric) => {
   //   console.log(metric);
   // });
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-background">
       <Suspense fallback={<Loading />}>
-        <SplineScene />
+        <SplineScene_ssr />
       </Suspense>
-      {/* Hero Title and Subtitle */}
+      {/* Hero Title and Subtitle with SSR */}
       <Hero />
     </div>
   );
