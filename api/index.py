39aa5abb1e9130e4da9561ffd759718632c06ac8
jayseregon <jayseregon@gmail.com>
2025-02-11 09:39:47 -0500

    fix python import path

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from services.keywordEmbedding import EmbeddingService
+from .services.keywordEmbedding import EmbeddingService
 
 app = FastAPI()
 
